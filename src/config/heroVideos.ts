0b7ea922f5a46d601297edb58c516be15d13cbb0
Andrei Yushkevich <yushkevich@me.com>
2026-01-23 19:00:36 +0100

    new media

--- src/config/heroVideos.ts
+++ src/config/heroVideos.ts
@@ -9,31 +9,31 @@ export const heroVideos: HeroVideoConfig[] = [
   {
     srcMp4: '/media/hero/hero1.mp4',
     srcWebm: '/media/hero/hero1.webm',
-    poster: '/media/hero/hero1-poster.png',
+    poster: '/media/hero/hero1-poster.jpg',
     duration: 1,
   },
   {
     srcMp4: '/media/hero/hero2.mp4',
     srcWebm: '/media/hero/hero2.webm',
-    poster: '/media/hero/hero2-poster.png',
+    poster: '/media/hero/hero2-poster.jpg',
     duration: 4,
   },
   {
     srcMp4: '/media/hero/hero3.mp4',
     srcWebm: '/media/hero/hero3.webm',
-    poster: '/media/hero/hero3-poster.png',
+    poster: '/media/hero/hero3-poster.jpg',
     duration: 4,
   },
   {
     srcMp4: '/media/hero/hero4.mp4',
     srcWebm: '/media/hero/hero4.webm',
-    poster: '/media/hero/hero4-poster.png',
+    poster: '/media/hero/hero4-poster.jpg',
     duration: 1,
   },
   {
     srcMp4: '/media/hero/hero5.mp4',
     srcWebm: '/media/hero/hero5.webm',
-    poster: '/media/hero/hero5-poster.png',
+    poster: '/media/hero/hero5-poster.jpg',
     duration: 5,
   },
 ];
